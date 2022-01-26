49ca21fbdc399fdfe3d93ba52e12fe0bdeee9edb
CeciliaDenise96 <98282794+CeciliaDenise96@users.noreply.github.com>
2022-01-26 10:19:55 -0300

    Programa en C para opción de semestre, carrera.
    Muestra las materias de la carrera a elegir

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
-#include<stdio.h>
-
-void funcion_area_triangulo()
-{
-    int base,altura,resultado1;
-    printf("Ingrese los siguientes datos: \n");
-    printf("Ingrese el valor de la base \n ");
-    scanf("%i", &base);
-    printf("Ingrese el valor de la altura \n ");
-    scanf("%i", &altura);
-    resultado1 = (base*altura)/2;
-    
-    printf("El área del triángulo es: %i \n", resultado1);
-}
+#include <stdio.h>
 
 void main()
 {
-    printf("Bienvenido al programa que calcula el área del triángulo \n");
-    funcion_area_triangulo();
+    int num_semestre,opcion;
+    printf("Ingresa tu semestre \n");
+    scanf("%i", &num_semestre);
+    if(num_semestre>3)
+    {
+        printf("Bienvenido al sistema de consulta de horarios escolares online \n");
+        printf("Elige la carrera que cursas \n");
+        printf(" 1: Ingeniería en computación, \n ");
+        printf(" 2: Ingeniería mecánica \n");
+        printf("Opción : \n");
+        scanf("%i",&opcion);
+        switch(opcion)
+        {
+            case 1:
+            printf("Cuarto semestre \n Análisis de circuitos electrónicos \n Estructuras discretas \n Programación de sistemas \n");
+            break;
+            case 2:
+            printf("Cuarto semestre \n Ciencias y tecnología de materiales \n Fundamentos de mécanica de sólidos \n Mecánica de Fluidos \n");
+            break;
+            default:("Opción no valida \n");
+        }
+    }
+    else printf("En el sistema sólo están cargados los horarios escolares a partir de cuarto semestre \n");
     getchar();
 }
