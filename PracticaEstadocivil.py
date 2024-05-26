abcaf8c698ee76c7a34be4562887935331186b6b
CeciliaDenise96 <98282794+CeciliaDenise96@users.noreply.github.com>
2024-05-26 16:05:29 -0300

    Update PracticaEstadocivil.py

--- PracticaEstadocivil.py
+++ PracticaEstadocivil.py
@@ -2,21 +2,21 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
+def obtener_texto():
+    texto_ingresado = cuadro_texto.get()
+    etiqueta.config(text="Texto ingresado: " + texto_ingresado)
 ventana = tk.Tk()
 imagen = Image.open("estadocivil.png")
 imagen_tk = ImageTk.PhotoImage(imagen)
 etiqueta = tk.Label(ventana, image=imagen_tk)
 etiqueta.pack()
-def obtener_texto():
-    texto_ingresado = cuadro_texto.get()
-    etiqueta.config(text="Texto ingresado: " + texto_ingresado)
-    
-etiqueta = tk.Label(ventana, text="Ingrese su estado civil:", bg="white", fg="black", font = ("Arial", 16), width = 20 , height= 2 , anchor = "center")
+
+etiqueta = tk.Label(ventana, text="Texto ingresado:")
 etiqueta.pack()
 cuadro_texto = tk.Entry(ventana, width = 30)
 cuadro_texto.pack()
 
-boton = tk.Button(ventana, text="Estado: ", command="obtener_texto")
+boton = tk.Button(ventana, text="Obtener texto ", command=obtener_texto)
 boton.pack()
 
 ventana.mainloop()
