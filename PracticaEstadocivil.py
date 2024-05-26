0dba599eafa49cfe9d0d39c608878505376dbe6f
CeciliaDenise96 <98282794+CeciliaDenise96@users.noreply.github.com>
2024-05-26 12:04:02 -0300

    Update PracticaEstadocivil.py
    Include image

--- PracticaEstadocivil.py
+++ PracticaEstadocivil.py
@@ -1,11 +1,16 @@
 # -*- coding: latin-1 -*-
 import tkinter as tk
+from PIL import ImageTk, Image
 
 ventana = tk.Tk()
+imagen = Image.open("estadocivil.png")
+imagen_tk = ImageTk.PhotoImage(imagen)
+etiqueta = tk.Label(ventana, image=imagen_tk)
+etiqueta.pack()
 def obtener_texto():
     texto_ingresado = cuadro_texto.get()
     etiqueta.config(text="Texto ingresado: " + texto_ingresado)
-
+    
 etiqueta = tk.Label(ventana, text="Ingrese su estado civil:", bg="white", fg="black", font = ("Arial", 16), width = 20 , height= 2 , anchor = "center")
 etiqueta.pack()
 cuadro_texto = tk.Entry(ventana, width = 30)
