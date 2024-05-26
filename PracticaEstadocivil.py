94631422210a4a1ce9c6a8a2945b1cfef9807661
CeciliaDenise96 <98282794+CeciliaDenise96@users.noreply.github.com>
2024-05-26 16:32:45 -0300

    Update PracticaEstadocivil.py

--- PracticaEstadocivil.py
+++ PracticaEstadocivil.py
@@ -1,17 +1,18 @@
 # -*- coding: latin-1 -*-
 import tkinter as tk
 from PIL import ImageTk, Image
-
+ventana = tk.Tk()
+marco1 = tk.Frame(ventana, width=200, height=100, bd=2, relief = "solid")
+marco1.pack()
 def obtener_texto():
     texto_ingresado = cuadro_texto.get()
     etiqueta.config(text="Texto ingresado: " + texto_ingresado)
-ventana = tk.Tk()
 imagen = Image.open("estadocivil.png")
 imagen_tk = ImageTk.PhotoImage(imagen)
-etiqueta = tk.Label(ventana, image=imagen_tk)
+etiqueta = tk.Label(marco1, image=imagen_tk)
 etiqueta.pack()
 
-etiqueta = tk.Label(ventana, text="Texto ingresado:")
+etiqueta = tk.Label(marco1, text="Texto ingresado:")
 etiqueta.pack()
 cuadro_texto = tk.Entry(ventana, width = 30)
 cuadro_texto.pack()
@@ -19,4 +20,24 @@ cuadro_texto.pack()
 boton = tk.Button(ventana, text="Obtener texto ", command=obtener_texto)
 boton.pack()
 
+def obtener_seleccion():
+    seleccionados = cuadro_lista.curselection()
+    for index in seleccionados:
+        elemento = cuadro_lista.get(index)
+        print("Elemento seleccionado:", elemento)
+
+ventana = tk.Tk()
+
+cuadro_lista = tk.Listbox(ventana, width=30, selectmode="multiple")
+cuadro_lista.pack()
+
+elementos = ["Soltero", "Casado", "En pareja", "Con concentimiento"]
+
+for elemento in elementos:
+    cuadro_lista.insert(tk.END, elemento)
+
+boton = tk.Button(ventana, text="Obtener", command=obtener_seleccion)
+boton.pack()
+
 ventana.mainloop()
+
