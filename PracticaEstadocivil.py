1402719bdc9246c76b8ae23b0d2b6f179ed96e0d
CeciliaDenise96 <98282794+CeciliaDenise96@users.noreply.github.com>
2024-05-27 19:06:08 -0300

    Update finish PracticaEstadocivil.py

--- PracticaEstadocivil.py
+++ PracticaEstadocivil.py
@@ -2,11 +2,12 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 ventana = tk.Tk()
+ventana.geometry("1500x1400")
 marco1 = tk.Frame(ventana, width=200, height=100, bd=2, relief = "solid")
 marco1.pack()
 def obtener_texto():
-    texto_ingresado = cuadro_texto.get()
-    etiqueta.config(text="Texto ingresado: " + texto_ingresado)
+        texto_ingresado = cuadro_texto.get()
+        etiqueta.config(text="Texto ingresado: " + texto_ingresado)
 imagen = Image.open("estadocivil.png")
 imagen_tk = ImageTk.PhotoImage(imagen)
 etiqueta = tk.Label(marco1, image=imagen_tk)
@@ -24,9 +25,8 @@ def obtener_seleccion():
     seleccionados = cuadro_lista.curselection()
     for index in seleccionados:
         elemento = cuadro_lista.get(index)
-        print("Elemento seleccionado:", elemento)
+    print("Elemento seleccionado:", elemento)
 
-ventana = tk.Tk()
 
 cuadro_lista = tk.Listbox(ventana, width=30, selectmode="multiple")
 cuadro_lista.pack()
@@ -40,12 +40,11 @@ boton = tk.Button(ventana, text="Obtener", command=obtener_seleccion)
 boton.pack()
 
 def obtener_estado():
-    if variable.get() == 1:
-        print("La casilla de verificacion esta seleccionada")
-    else:
-        print("La casilla de verificacion no esta seleccionada")
+   if variable.get() == 1:
+      print("La casilla de verificacion esta seleccionada")
+   else:
+      print("La casilla de verificacion no esta seleccionada")
 
-ventana = tk.Tk()
 
 variable = tk.IntVar()
 
